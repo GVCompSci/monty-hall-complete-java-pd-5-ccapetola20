ae2806f8c30354d5b18d742b5f20fa6166e0807d
ccapetola20 <42975602+ccapetola20@users.noreply.github.com>
2019-01-04 12:38:50 -0500

    Add files via upload

---
+++ src/MontyHall.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+import java.util.Random;
+/**
+ * 
+ * @author Casey Capetola
+ */
+public class MontyHall 
+{
+
+	public static void main(String[] args) 
+	{
+		Random rand = new Random();
+		Scanner user = new Scanner(System.in);
+		
+		int winCount = 0;
+		int lossCount = 0;
+		System.out.print("How many rounds of the game should be simulated: ");
+		int rounds = Integer.parseInt(user.nextLine());
+		while(rounds<10 || rounds>10000)
+		{
+			System.out.println("\nMust enter a number between 10 and 10000");
+			System.out.print("How many rounds of the game should be simulated: ");
+			rounds = Integer.parseInt(user.nextLine());
+		}
+		
+		System.out.print("Should the player switch or stay? ");
+		String choice = user.nextLine();
+		while(!choice.equals("switch") && !choice.equals("stay"))
+		{
+			System.out.println("Must enter either \"switch\" or \"stay\"");
+			System.out.print("Should the player switch or stay? ");
+			choice = user.nextLine();
+		}
+		
+		int hidDoor;
+		int doorOne;
+		
+		if(choice.equals("switch"))
+		{
+			for(int i= 0; i < rounds; i++)
+			{
+				hidDoor = rand.nextInt(3) + 1;
+				doorOne = rand.nextInt(3) + 1;
+				
+				if(hidDoor==doorOne)
+				{
+					lossCount++;
+				}	
+				else
+				{
+					winCount++;
+				}		
+			}
+			double winPercent = (winCount*100.0)/rounds;
+			System.out.printf("\nThe player won %d/%d games OR %.2f", winCount, rounds, winPercent);
+			System.out.print("% of games played!");
+		}
+		else if(choice.equals("stay"))
+		{
+			for(int i=0; i < rounds; i++)
+			{
+				hidDoor = rand.nextInt(3) + 1;
+				doorOne = rand.nextInt(3) + 1;
+				
+				if(hidDoor==doorOne)
+				{
+					winCount++;
+				}
+				else
+				{
+					lossCount++;
+				}
+			}
+			double winPercent = (winCount*100.0)/rounds;
+			System.out.printf("\nThe player won %d/%d games OR %.2f", winCount, rounds, winPercent);
+			System.out.print("% of games!");
+		}
+		else
+		{
+			System.out.print("\nERROR");
+		}
+
+	}
+
+}
